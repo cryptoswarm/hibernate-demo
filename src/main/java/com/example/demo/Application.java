73052a24229930f31f344815ce22549b4baaa89c
Mokhtar Safir <mokhtarsafir@Mokhtars-MacBook-Pro-2.local>
2026-07-18 15:07:03 -0400

    add student repository

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +12,16 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student foo = new Student(
+                    "foo",
+                    "bar",
+                    "foo@bar.com",
+                    25
+            );
+            studentRepository.save(foo) ;
+        };
+    }
 }
